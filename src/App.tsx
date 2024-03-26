729ddb659d29b8498e13d92fcaef748f964c9835
Nguyen Kim Huy <kimhuy011199@gmail.com>
2024-03-26 09:24:12 +0700

    Init source code

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import InputContainer from './components/InputContainer';
+
+const App = () => {
+  return (
+    <div className="h-screen w-screen">
+      <InputContainer />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/InputContainer.tsx
@@ -0,0 +1,9 @@
+const InputContainer = () => {
+  return (
+    <div className="fixed bottom-0 w-screen flex justify-center items-end bg-red-500">
+      InputContainer
+    </div>
+  );
+};
+
+export default InputContainer;
