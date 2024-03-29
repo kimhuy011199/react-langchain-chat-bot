f7549ff5626244fa403f30ec8f4bdc37278c31da
Nguyen Kim Huy <kimhuy011199@gmail.com>
2024-03-29 23:49:41 +0700

    Create about project dialog

--- src/components/AboutProject.tsx
+++ src/components/AboutProject.tsx
@@ -1,13 +1,27 @@
+import { ReactNode } from 'react';
+import { CircleHelp, Link as LinkIcon } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 import {
   Dialog,
   DialogContent,
-  DialogDescription,
   DialogHeader,
-  DialogTitle,
   DialogTrigger,
 } from '@/components/ui/dialog';
-import { CircleHelp } from 'lucide-react';
+import AppLogo from './AppLogo';
+import LangChainIcon from './icons/LangChainIcon';
+import { ReactIcon } from './icons/ReactIcon';
+import NodejsIcon from './icons/NodejsIcon';
+
+const Link = ({ href, children }: { href: string; children: ReactNode }) => {
+  return (
+    <a
+      className="px-3.5 py-2.5 text-sm flex items-center gap-3 rounded-md font-medium transition-colors border border-ring bg-background hover:bg-muted hover:text-muted-foreground border-dashed"
+      href={href}
+    >
+      {children}
+    </a>
+  );
+};
 
 const AboutProject = () => {
   return (
@@ -17,12 +31,49 @@ const AboutProject = () => {
           <CircleHelp size={18} />
         </Button>
       </DialogTrigger>
-      <DialogContent className="sm:max-w-md">
+      <DialogContent className="sm:max-w-lg">
         <DialogHeader>
-          <DialogTitle>ChatLangChain</DialogTitle>
-          <DialogDescription>About ChatLangChain</DialogDescription>
+          <AppLogo />
         </DialogHeader>
-        <div></div>
+        <div className="text-sm font-medium flex flex-col gap-3">
+          <p>
+            ChatLangChain is an innovative chatbot web application powered by a
+            large language model (GPT), intelligently trained on my portfolio
+            data to deliver personalized responses instantly. You can ask this
+            chatbot about my profile and receive AI-powered answers.
+          </p>
+          <ul className="list-disc ml-4 flex flex-col gap-2">
+            <li>
+              Utilize AI technology to generate answers based on trained data.
+            </li>
+            <li>
+              Copy and regenerate answers as needed for enhanced convenience.
+            </li>
+            <li>Customize viewing experience with light/dark mode options.</li>
+          </ul>
+          <div className="flex items-center gap-4">
+            <span className="mr-2">Tech stack:</span>
+            <a href="https://www.langchain.com/">
+              <LangChainIcon />
+            </a>
+            <a href="https://react.dev/">
+              <ReactIcon />
+            </a>
+            <a href="https://nodejs.org/en">
+              <NodejsIcon />
+            </a>
+          </div>
+          <div className="flex items-center gap-3">
+            <Link href="https://github.com/kimhuy011199/react-langchain-chat-bot">
+              <LinkIcon size={14} />
+              <span>Github Repo</span>
+            </Link>
+            <Link href="https://github.com/kimhuy011199/react-langchain-chat-bot">
+              <LinkIcon size={14} />
+              <span>My Portfolio</span>
+            </Link>
+          </div>
+        </div>
       </DialogContent>
     </Dialog>
   );
